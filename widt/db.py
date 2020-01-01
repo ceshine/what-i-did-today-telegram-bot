bcc4738cc76b7c661a9f611815b81f9ada0d1d27
Ceshine Lee <shuanck@gmail.com>
2020-01-01 13:55:43 +0800

    Add TEST_MODE: don't actually create a Firestore client

--- widt/db.py
+++ widt/db.py
@@ -3,4 +3,8 @@ import os
 from google.cloud import firestore
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keyfile.json"
+
+if os.environ.get("TEST_MODE"):
+    DB = None
+
 DB = firestore.Client()
